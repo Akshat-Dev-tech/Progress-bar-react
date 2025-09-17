4a9ecc3c4e52d215b520e9d92d1a0e7711a4f7c9
Akshat Patel <akiiwork1234@gmail.com>
2025-09-17 19:12:17 +0000

    simple prgress bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./styles.css";
 
 export default function App() {
+  const getProgress = (prgressVal) => {
+    return (
+      <div className="outer">
+        <div className="Inner" style={{ width: `${prgressVal}%` }}>
+          {prgressVal}%
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <h1>Progress Bar</h1>
+      {getProgress(70)}
     </div>
   );
 }
