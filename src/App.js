f7295a5dee677fa3b95e321634c0e0648a977eb1
Akshat Patel <akiiwork1234@gmail.com>
2025-09-18 19:59:11 +0000

    progress bar added transition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./styles.css";
-
+import {useState,useEffect} from 'react'
 export default function App() {
   const getProgress = (prgressVal) => {
+    const [progress , setProgress] = useState(0)
+    useEffect(()=>{
+      setTimeout(() => {
+        setProgress(prgressVal)
+      }, 1000)
+    },[progress])
     return (
       <div className="outer">
-        <div className="Inner" style={{ width: `${prgressVal}%` }}>
-          {prgressVal}%
+        <div>AK</div>
+        <div className="Inner" style={{ width: `${progress}%` }}>
+          {progress}%
         </div>
       </div>
     );
@@ -14,7 +21,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Progress Bar</h1>
-      {getProgress(70)}
+      {getProgress(20)}
     </div>
   );
 }
